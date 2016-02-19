9b11353e600f85fa76e0e25bc517194fec58a79a
Sven van Steenis <sven.vsteenis@gmail.com>
2016-02-19 16:05:52 +0100

    Added simple nav bar for single page navigation

--- js/core.js
+++ js/core.js
@@ -3,4 +3,18 @@ jQuery(document).ready(function($){
 	// Responsive header resizing using fittext.js
 	$(".title").fitText(1, { minFontSize: '42px', maxFontSize: '82px' });
 
+
+	// Nav btn functions to scroll to section
+	$(".nav-btn").click(function(e) {
+		e.preventDefault();
+
+		var elem = $(this).attr('href');
+
+		if($(elem).length) {
+			$('html, body').animate( { scrollTop: $(elem).offset().top }, 800 );
+		} else {
+			return false;
+		}
+	});
+
 });
