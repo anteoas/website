0cc2459c5666bc8bb7fb73a108142a4575ea2ec9
Sven van Steenis <sven.vsteenis@gmail.com>
2016-02-19 14:35:04 +0100

    Dropped JS header resizing in favor of vh viewport unit

--- js/core.js
+++ js/core.js
@@ -1,19 +1,5 @@
 jQuery(document).ready(function($){
 
-	// Make intro section responsive
-	resize_intro();
-	$(window).resize(function(){
-		resize_intro();
-	});
-
-	// Function to resize the intro section
-	function resize_intro() {
-		var window_height = $(window).height();
-		var intro_height = window_height * 0.6;
-
-		$("#intro").css('height', intro_height);
-	}
-
 	// Responsive header resizing using fittext.js
 	$(".title").fitText(1, { minFontSize: '42px', maxFontSize: '82px' });
 
