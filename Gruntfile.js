0f366b94f86778294f2651d3a7a3c35d44c581a7
Sven van Steenis <sven.vsteenis@gmail.com>
2016-02-24 11:29:05 +0100

    Reconfigured Grunt for better minification; added minified files to index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,35 +3,51 @@ module.exports = function(grunt) {
     // 1. All configuration goes here 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        // Minify scripts
+        // Compile & minify SASS
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'css/style.css': 'scss/style.scss',
+                }
+            } 
+        },
+        // Minify CSS
+		cssmin: {
+			options: {
+				sourceMap: true
+			},
+			target: {
+				files: [{
+					expand: true,
+					cwd: 'css',
+					src: ['*.css', '!*.min.css'],
+					dest: 'css',
+					ext: '.min.css'
+				}]
+			}
+		},
+        // Uglify scripts
         uglify: {
             build: {
                 options: {
                     sourceMap: true
                 },
                 files: {
-                  'js/core.min.js': ['js/core.js']
+                  'js/core.min.js': ['js/core.js'],
+                  'js/fittext.min.js': ['js/fittext.js']
                 }
             }
         },    
-        // Compile & minify SASS
-        sass: {
-            dist: {
-                options: {
-                    style: 'compressed'
-                },
-                files: {
-                    'css/style.min.css': 'scss/style.scss',
-                }
-            } 
-        }, 
         // Watch for changes and run tasks on change
         watch: {
             options: {
                 livereload: true
             },            
             scripts: {
-                files: ['js/*.js'],
+                files: ['js/*.js', 'css/*.css'],
                 tasks: ['uglify'],
                 options: {
                     spawn: false
@@ -49,11 +65,12 @@ module.exports = function(grunt) {
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['uglify', 'sass', 'watch']);
+    grunt.registerTask('default', ['sass', 'cssmin', 'uglify', 'watch']);
 
 };
